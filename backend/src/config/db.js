bb5eab9ddba8f871bbee3c9971cd2715cecc5d7d
182slash <juliosilahi@gmail.com>
2026-04-30 13:48:44 +0700

    AddJob 0.0.1

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI
+  const uri = process.env.MONGO_URI_NEW || process.env.MONGO_URI
   if (!uri) throw new Error('MONGO_URI is not defined in .env')
 
   try {
